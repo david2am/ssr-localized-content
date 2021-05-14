d093c3a72b064af9fbb19d85a513fa54cc03551a
David De Vargas <David.DeVargas@endava.com>
2021-05-14 16:47:52 -0500

    rewrite languages

--- src/_data/news.js
+++ src/_data/news.js
@@ -1,14 +1,26 @@
 const axios = require('axios')
+const countries = require('./countries.json')
+
 require('dotenv').config()
 
 const apiKey = process.env.NEWS_API_KEY
 const baseURL = 'https://newsapi.org/v2/top-headlines'
 
-module.exports = async function () {
+async function getNews (country) {
   try {
-    const response = await axios.get(`${baseURL}?country=us&apiKey=${apiKey}&pageSize=5`)
-    return response.data
+    const response = await axios.get(`${baseURL}?country=${country}&apiKey=${apiKey}&pageSize=5`)
+    return {
+      country,
+      articles: response.data.articles
+    }
   } catch (e) {
     console.error(e)
   }
+}
+
+module.exports = async function () {
+  var newsPromises = countries.map(getNews)
+  return Promise.all(newsPromises).then(newsObjects => {
+    return [].concat.apply([], newsObjects)
+  })
 }
